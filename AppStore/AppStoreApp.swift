3c54efdb984044bd71329afa0bd7761d98a83482
Berkay-Disli <berkaydisli.work@gmail.com>
2022-04-15 20:11:04 +0300

    Initial Commit

---
+++ AppStore/AppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreApp.swift
+//  AppStore
+//
+//  Created by Berkay Disli on 15.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
